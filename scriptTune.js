277b2aee232084ad53e349b66d772ebcb7701e93
Wout Van Damme <wote14@gmail.com>
2019-11-06 13:46:57 +0100

    shake

--- scriptTune.js
+++ scriptTune.js
@@ -14,11 +14,15 @@ function makeShake()
 
 
 if (newValue1 != oldValue1) {
-    oldValue1 = newValue1;
-    var el = document.getElementById("firstIcon");
+
+	if (newValue1 < oldValue1){
+	var el = document.getElementById("firstIcon");
+	el.className = "md icon-small hydrated icons";
+	}
+	else{
     var el2 = document.getElementById("firstIcon2");
-    el.className = "md icon-small hydrated icons";
-    el2.className = "md icon-large hydrated icons";
+    el2.className = "md icon-large hydrated icons";}
+	oldValue1 = newValue1;
     var el = document.getElementById("button1");
     el.innerHTML = oldValue1;
 } else {
@@ -30,11 +34,16 @@ if (newValue1 != oldValue1) {
 
 
 if (newValue2 != oldValue2) {
-    oldValue2 = newValue2;
-    var el = document.getElementById("secondIcon");
+if (newValue2 < oldValue2){
+ var el = document.getElementById("secondIcon");
     el.className = "md icon-small hydrated icons";
-    var el2 = document.getElementById("secondIcon2");
+}else{
+  var el2 = document.getElementById("secondIcon2");
     el2.className = "md icon-large hydrated icons";
+}
+    
+   
+  oldValue2 = newValue2;
     var el = document.getElementById("button2");
     el.innerHTML = oldValue2;
 } else {
@@ -45,11 +54,17 @@ el2.className = "md icon-large hydrated";
 }
 
 if (newValue3 != oldValue3) {
-    oldValue3 = newValue3;
-    var el = document.getElementById("thirdIcon");
+if (newValue3 < oldValue3){
+var el = document.getElementById("thirdIcon");
     el.className = "md icon-small hydrated icons";
-    var el2 = document.getElementById("thirdIcon2");
+}
+else{
+ var el2 = document.getElementById("thirdIcon2");
     el2.className = "md icon-large hydrated icons";
+}
+    oldValue3 = newValue3;
+    
+   
     var el = document.getElementById("button3");
     el.innerHTML = oldValue3;
 } else {
