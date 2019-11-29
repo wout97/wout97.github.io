cae8d35bcf4f0e68e58b6182a1d6253e6bff5cce
Wout Van Damme <wote14@gmail.com>
2019-11-29 11:43:19 +0100

    experiment preview filtering

--- scriptFinal.js
+++ scriptFinal.js
@@ -36,9 +36,9 @@ $(() => {
 				featuresOfTracks = featuresData;
 				tracks.forEach((track, i) => {
 					var features = featuresData.audio_features[i];
-					var hidden = false;
-					if(track.preview_url){hidden = true};
-				
+					var hidden = true;
+					if(track.preview_url){hidden = false};
+					console.log(track.preview_url);
 					$('#recommendations').append(`
 <div id='track${ i }' hidden='${ hidden }'>
 	<div class="container center">
