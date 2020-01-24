b67efc7967fc4732204fe0343deac58739c83f4c
Wout Van Damme <wote14@gmail.com>
2020-01-24 18:13:07 +0100

    pwa rocks

---
+++ criterium75/sw.js
@@ -0,0 +1,46 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  'index.html',
+  'app.js',
+  'js/script.js',
+  '/style/stylesheet.css',
+  '/img/Een.png',
+  '/img/Twee.png',
+  '/img/Drie.png',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css?family=Orbitron&display=swap&effect=outline',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://fonts.googleapis.com/css?family=VT323&display=swap'
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
